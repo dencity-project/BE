7b3f6c76ef583067b4a0fb2e0447f0b2bde47397
Jeongdonghun <arrrr180@konkuk.ac.kr>
2023-08-12 21:53:48 +0900

    github action

--- src/main/java/com/example/be/BeApplication.java
+++ src/main/java/com/example/be/BeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BeApplication.class, args);
     }
 
